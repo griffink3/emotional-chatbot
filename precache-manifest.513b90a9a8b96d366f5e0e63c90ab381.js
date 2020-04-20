ae831f59837f66617ef2bdbf6ccab59316530db3
Griffin <griffink3@gmail.com>
2020-04-20 13:45:55 -0400

    Updates

---
+++ precache-manifest.513b90a9a8b96d366f5e0e63c90ab381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1ef6d11d4a50ba14c8c4b71d00bb52",
+    "url": "/emotional-chatbot/index.html"
+  },
+  {
+    "revision": "0aba360275e17f638b12",
+    "url": "/emotional-chatbot/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e19ea3a3151f50d8ffe1",
+    "url": "/emotional-chatbot/static/js/2.d6651b8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emotional-chatbot/static/js/2.d6651b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aba360275e17f638b12",
+    "url": "/emotional-chatbot/static/js/main.5cf45238.chunk.js"
+  },
+  {
+    "revision": "2b4f6925be905d386955",
+    "url": "/emotional-chatbot/static/js/runtime-main.a55fa3cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emotional-chatbot/static/media/logo.5d5d9eef.svg"
+  }
+]);
